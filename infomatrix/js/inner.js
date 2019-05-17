62fd1742a762fda729d94a240e4301e9331cc1d2
Sergey Romanyuk <34944406+romanyuchkovskiy@users.noreply.github.com>
2019-05-17 13:02:12 +0300

    Add files via upload

---
+++ infomatrix/js/inner.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+// 	 $("#main-carousel").owlCarousel({
+// 	 	items: 1,
+// 	 	loop: true,
+// 	 	nav: true,
+// 	  	navText: ['',''],
+// 	  	dots: true,
+// 	 });
+// });
