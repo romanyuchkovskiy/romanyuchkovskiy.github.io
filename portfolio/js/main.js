1f535e171edac0b088aeab1f5c3d1d3f64f2d40f
Sergey Romanyuk <34944406+romanyuchkovskiy@users.noreply.github.com>
2019-07-02 17:16:58 +0300

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("a[href*='#']").mPageScroll2id();
+
+	// $("#order-layout").on('click', function(){
+	// 	$("#modal-1").show();
+	// });
+	// $("#order-build").on('click', function(){
+	// 	$("#modal-2").show();
+	// });
+	// $(".modal-close").on('click', function(){
+	// 	$("#modal-1").hide();
+	// 	$("#modal-2").hide();
+	// });
+});
