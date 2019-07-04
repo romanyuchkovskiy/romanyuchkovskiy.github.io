1560dc2fc56739d9adbf1c3a99727d845bc73a79
Sergey Romanyuk <34944406+romanyuchkovskiy@users.noreply.github.com>
2019-07-04 14:37:44 +0300

    Адаптив модалок

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 
 	$("#order-layout").on('click', function(){
 		$("#modal-1").show();
-	 });
-	 $("#order-build").on('click', function(){
+	});
+	$("#order-build").on('click', function(){
 		$("#modal-2").show();
-	 });
-	 $(".modal-close").on('click', function(){
-	 	$("#modal-1").hide();
-	 	$("#modal-2").hide();
-	 });
+	});
+	$(".modal-close").on('click', function(){
+		$("#modal-1").hide();
+		$("#modal-2").hide();
+	});
 });
