a1df735671ae12cbf9fa48c6261c6da0aeb00996
Sergey Romanyuk <34944406+romanyuchkovskiy@users.noreply.github.com>
2019-07-06 12:05:27 +0300

    Add files via upload

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 	$("a[href*='#']").mPageScroll2id();
 
-	$("#order-layout").on('click', function(){
-		$("#modal-1").show();
-	});
-	$("#order-build").on('click', function(){
-		$("#modal-2").show();
-	});
-	$(".modal-close").on('click', function(){
-		$("#modal-1").hide();
-		$("#modal-2").hide();
+	// $("#order-layout").on('click', function(){
+	// 	$("#modal-1").show();
+	// 	$(".overlay").show();
+	// });
+	// $("#order-build").on('click', function(){
+	// 	$("#modal-2").show();
+	// 	$(".overlay").show();
+	// });
+	// $(".modal-close").on('click', function(){
+	// 	$("#modal-1").hide();
+	// 	$("#modal-2").hide();
+	// 	$(".overlay").hide();
+	// });
+
+	var rellax = new Rellax('.rellax', {
+		speed: -3,
+	    center: false,
+	    round: true,
+	    vertical: true,
+	    horizontal: false
 	});
 });
